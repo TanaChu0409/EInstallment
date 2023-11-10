da8b321f879d3036c90d24f22c5c3ff54d89e567
alvin.chu <alvin.chu@cloudysys.com>
2023-11-10 23:05:00 +0800

    #12, add missing

--- EInstallment.Domain/Shared/ResultExtensions.cs
+++ EInstallment.Domain/Shared/ResultExtensions.cs
@@ -27,20 +27,20 @@ public static class ResultExtensions
     public static T Match<T>(
         this Result result,
         Func<T> onSuccess,
-        Func<Error, T> onFailure)
+        Func<Result, T> onFailure)
     {
         return result.IsSuccess ?
                 onSuccess() :
-                onFailure(result.Error);
+                onFailure(result);
     }
 
     public static T Match<TValue, T>(
         this Result<TValue> result,
         Func<TValue, T> onSuccess,
-        Func<Error, T> onFailure)
+        Func<Result, T> onFailure)
     {
         return result.IsSuccess ?
                 onSuccess(result.Value) :
-                onFailure(result.Error);
+                onFailure(result);
     }
 }
